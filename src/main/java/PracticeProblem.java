e383bf10f3e2fac2e4e0631567aee1a9ea699c7a
jamin-xie-ics4u1-02-25 <Jamin.xie@student.tdsb.on.ca>
2025-03-05 16:03:47 +0000

    Update Change

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,30 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
 	
+	public static boolean validIndex(int[] intArray, int index) {
+		try {
+			int temp = intArray[index];
+			return true;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return false;
+		}
+	}
+
+	public static int divide(int a, int b) {
+		try {
+			return a / b;
+		} catch (ArithmeticException e) {
+			return 0;
+		}
+	}
+
+	public static int safeConvertStringtoInt(String word) {
+		try {
+			return Integer.parseInt(word);
+		} catch (NumberFormatException e) {
+			return 0;
+		}
+	} 
 
 }
